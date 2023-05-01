835e7d4bba05b81c8c939a31e7f5c24609d58372
zdnniz <zdennis076@gmail.com>
2023-05-01 17:00:14 +0800

    initial commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index),
 ]
